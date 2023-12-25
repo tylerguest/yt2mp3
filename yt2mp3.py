02654c5286678e527b8c04d37513b0de67d7c666
Tyler Guest <83128087+tylerguest@users.noreply.github.com>
2023-12-24 20:10:51 -0500

    added functionality
    now takes user input for youtube url and auto generates .mp3 title based on the title of the youtube video

--- yt2mp3.py
+++ yt2mp3.py
@@ -2,19 +2,21 @@ import os
 from pytube import YouTube
 from pydub import AudioSegment
 
-def download_and_convert_to_mp3(video_url, output_filename):
+def download_and_convert_to_mp3(video_url):
     try:
         # Download the YouTube video
         youtube = YouTube(video_url)
+        video_title = youtube.title  # Extract the video title
+
         video_stream = youtube.streams.filter(only_audio=True).first()
-        video_stream.download(filename=f'{output_filename}.mp4')
+        video_stream.download(filename=f'{video_title}.mp4')
 
         # Convert the video to MP3 using pydub
-        audio = AudioSegment.from_file(f'{output_filename}.mp4', format="mp4")
-        audio.export(f'{output_filename}.mp3', format="mp3")
+        audio = AudioSegment.from_file(f'{video_title}.mp4', format="mp4")
+        audio.export(f'{video_title}.mp3', format="mp3")
 
         # Delete the mp4 file
-        os.remove(f'{output_filename}.mp4')
+        os.remove(f'{video_title}.mp4')
 
         print("Video downloaded, converted to MP3, and mp4 file deleted successfully!")
 
@@ -22,10 +24,7 @@ def download_and_convert_to_mp3(video_url, output_filename):
         print(f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    # Replace "YOUR_VIDEO_URL" with the actual YouTube video URL
-    video_url = "https://www.youtube.com/watch?v=76U0gtuV9AY&ab_channel=ChromeforDevelopers"
+    # Take user input for the YouTube video URL
+    video_url = input("Enter the YouTube video URL: ")
 
-    # Replace "output" with the desired output filename
-    output_filename = "output"
-
-    download_and_convert_to_mp3(video_url, output_filename)
+    download_and_convert_to_mp3(video_url)
