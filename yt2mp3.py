e636b5bd76ba0661c55bb25fba5c3bd23da93b9d
Tyler Guest <83128087+tylerguest@users.noreply.github.com>
2023-12-24 20:20:35 -0500

    added basic gui

--- yt2mp3.py
+++ yt2mp3.py
@@ -1,8 +1,11 @@
 import os
+from tkinter import Tk, Label, Entry, Button, StringVar
 from pytube import YouTube
 from pydub import AudioSegment
 
-def download_and_convert_to_mp3(video_url):
+def download_and_convert_to_mp3():
+    video_url = url_entry.get()
+
     try:
         # Download the YouTube video
         youtube = YouTube(video_url)
@@ -18,13 +21,27 @@ def download_and_convert_to_mp3(video_url):
         # Delete the mp4 file
         os.remove(f'{video_title}.mp4')
 
-        print("Video downloaded, converted to MP3, and mp4 file deleted successfully!")
+        result_label.config(text="Video downloaded and converted to MP3 successfully!")
 
     except Exception as e:
-        print(f"Error: {str(e)}")
+        result_label.config(text=f"Error: {str(e)}")
 
-if __name__ == "__main__":
-    # Take user input for the YouTube video URL
-    video_url = input("Enter the YouTube video URL: ")
+# Create the main window
+root = Tk()
+root.title("YouTube to MP3 Converter")
 
-    download_and_convert_to_mp3(video_url)
+# Create and place widgets
+Label(root, text="Enter YouTube video URL:").grid(row=0, column=0, padx=10, pady=10)
+
+url_var = StringVar()
+url_entry = Entry(root, textvariable=url_var, width=40)
+url_entry.grid(row=0, column=1, padx=10, pady=10)
+
+convert_button = Button(root, text="Convert to MP3", command=download_and_convert_to_mp3)
+convert_button.grid(row=1, column=0, columnspan=2, pady=10)
+
+result_label = Label(root, text="")
+result_label.grid(row=2, column=0, columnspan=2, pady=10)
+
+# Start the main event loop
+root.mainloop()
